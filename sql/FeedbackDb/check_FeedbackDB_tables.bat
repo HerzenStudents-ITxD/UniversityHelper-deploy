8950f61f233e16be51c49563bb9b273d90cf6906
AnechkaDV <nusadavidova22122004@gmail.com>
2025-05-02 19:46:04 +0300

    fix db name

--- sql/FeedbackDb/check_FeedbackDB_tables.bat
+++ sql/FeedbackDb/check_FeedbackDB_tables.bat
@@ -1,22 +1,22 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set USER_DB_PASSWORD=User_1234
+set FEEDBACK_DB_PASSWORD=User_1234
 set CONTAINER=sqlserver_db
-set DATABASE=UserDB
+set DATABASE=FeedbackDB
 
 echo Checking Feedback Service tables...
 
 echo Feedback:
-docker exec -it %CONTAINER% /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P %USER_DB_PASSWORD% -d %DATABASE% -Q "SELECT * FROM Feedback"
+docker exec -it %CONTAINER% /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P %FEEDBACK_DB_PASSWORD% -d %DATABASE% -Q "SELECT * FROM Feedback"
 
 echo Images:
-docker exec -it %CONTAINER% /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P %USER_DB_PASSWORD% -d %DATABASE% -Q "SELECT * FROM Images"
+docker exec -it %CONTAINER% /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P %FEEDBACK_DB_PASSWORD% -d %DATABASE% -Q "SELECT * FROM Images"
 
 echo Types:
-docker exec -it %CONTAINER% /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P %USER_DB_PASSWORD% -d %DATABASE% -Q "SELECT * FROM Types"
+docker exec -it %CONTAINER% /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P %FEEDBACK_DB_PASSWORD% -d %DATABASE% -Q "SELECT * FROM Types"
 
 echo FeedbackTypes:
-docker exec -it %CONTAINER% /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P %USER_DB_PASSWORD% -d %DATABASE% -Q "SELECT * FROM FeedbackTypes"
+docker exec -it %CONTAINER% /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P %FEEDBACK_DB_PASSWORD% -d %DATABASE% -Q "SELECT * FROM FeedbackTypes"
 
 echo Done ✅
--- sql/FeedbackDb/check_FeedbackDB_tables.sh
+++ sql/FeedbackDb/check_FeedbackDB_tables.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-USER_DB_PASSWORD="User_1234"
+FEEDBACK_DB_PASSWORD="User_1234"
 CONTAINER="sqlserver_db"
-DATABASE="UserDB"
+DATABASE="FeedbackDB"
 
 echo "Checking Feedback Service tables..."
 
 echo "Feedback:"
-docker exec -it $CONTAINER /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $USER_DB_PASSWORD -d $DATABASE -Q "SELECT * FROM Feedback"
+docker exec -it $CONTAINER /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $FEEDBACK_DB_PASSWORD -d $DATABASE -Q "SELECT * FROM Feedback"
 
 echo "Images:"
-docker exec -it $CONTAINER /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $USER_DB_PASSWORD -d $DATABASE -Q "SELECT * FROM Images"
+docker exec -it $CONTAINER /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $FEEDBACK_DB_PASSWORD -d $DATABASE -Q "SELECT * FROM Images"
 
 echo "Types:"
-docker exec -it $CONTAINER /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $USER_DB_PASSWORD -d $DATABASE -Q "SELECT * FROM Types"
+docker exec -it $CONTAINER /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $FEEDBACK_DB_PASSWORD -d $DATABASE -Q "SELECT * FROM Types"
 
 echo "FeedbackTypes:"
-docker exec -it $CONTAINER /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $USER_DB_PASSWORD -d $DATABASE -Q "SELECT * FROM FeedbackTypes"
+docker exec -it $CONTAINER /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $FEEDBACK_DB_PASSWORD -d $DATABASE -Q "SELECT * FROM FeedbackTypes"
 
 echo "Done ✅"
